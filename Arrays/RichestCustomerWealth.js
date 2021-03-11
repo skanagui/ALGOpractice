4749ad8a026c6b8e8112bd18461d9f2f69fb931e
skanagui <s.kanagui@gmail.com>
2021-03-11 11:43:32 -0500

    damn

--- Arrays/RichestCustomerWealth.js
+++ Arrays/RichestCustomerWealth.js
@@ -25,25 +25,34 @@ Example 3:
 
 Input: accounts = [[2,8,7],[7,1,3],[1,9,5]]
 Output: 17
- 
 
-Constraints:
-
-m == accounts.length
-n == accounts[i].length
-1 <= m, n <= 50
-1 <= accounts[i][j] <= 100
-*/
-
+ //(proposed steps)
 //create variable for largest output 
 //iterate over account array using [i] for index
 //create a nested loop for integer values 
 //function for sum for each account and compare (possibly Math.max)
+//  variable for current output for iteration once inside the first loop 
 //return largest output 
+*/
 
 const maximumWealth = (accounts)=> {
-
+    let bigSum = 0
     
+    for(let i = 0; i < accounts.length; i++){
+       let currentSum = 0
+       for( let j = 0; j < accounts[i].length; j++){
+        currentSum += accounts[i][j]
+        // currentSum = accounts[i][j] + accounts[i][j-1]
+       }
+        bigSum = Math.max(bigSum,currentSum)
+    // console.log(accounts[i])
+    console.log(currentSum)
+    }
+
+    return bigSum
 };
 
-console.log(maximumWealth([[1,5],[7,3],[3,5]]))
+
+ //console.log(maximumWealth([[1,5],[7,3],[3,5]]))
+console.log(maximumWealth([2,8,7],[7,1,3],[1,9,5]))
+// console.log(maximumWealth([[1,2,3],[3,2,1]]))
