688cf0d3f5041eebfecb45c190b40daec0a5b49f
skanagui <s.kanagui@gmail.com>
2021-03-11 11:50:04 -0500

    fixed

--- Arrays/RichestCustomerWealth.js
+++ Arrays/RichestCustomerWealth.js
@@ -39,20 +39,19 @@ const maximumWealth = (accounts)=> {
     let bigSum = 0
     
     for(let i = 0; i < accounts.length; i++){
-       let currentSum = 0
+       let subArraySum = 0
        for( let j = 0; j < accounts[i].length; j++){
-        currentSum += accounts[i][j]
-        // currentSum = accounts[i][j] + accounts[i][j-1]
+        subArraySum += accounts[i][j]
        }
-        bigSum = Math.max(bigSum,currentSum)
+        bigSum = Math.max(bigSum,subArraySum)
     // console.log(accounts[i])
-    console.log(currentSum)
+    //console.log(subArraySum)
     }
 
     return bigSum
 };
 
 
- //console.log(maximumWealth([[1,5],[7,3],[3,5]]))
-console.log(maximumWealth([2,8,7],[7,1,3],[1,9,5]))
-// console.log(maximumWealth([[1,2,3],[3,2,1]]))
+console.log(maximumWealth([[1,5],[7,3],[3,5]]))
+console.log(maximumWealth([[2,8,7],[7,1,3],[1,9,5]]))
+console.log(maximumWealth([[1,2,3],[3,2,1]]))
