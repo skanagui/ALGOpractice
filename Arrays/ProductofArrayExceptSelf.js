cd926c4c59e9d5930723334928460b0b925046aa
skanagui <s.kanagui@gmail.com>
2021-03-16 14:01:39 -0400

    hello

---
+++ Arrays/ProductofArrayExceptSelf.js
@@ -0,0 +1,46 @@
+/*
+Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4]
+Output: [24,12,8,6]
+Example 2:
+
+Input: nums = [-1,1,0,-3,3]
+Output: [0,0,9,0,0]
+ 
+
+Constraints:
+
+2 <= nums.length <= 105
+-30 <= nums[i] <= 30
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+ 
+*/
+
+const productExceptSelf = (nums) => {
+
+    let answer = []
+    let left = 1
+    let right = 1
+
+    for (let i = 0; i < nums.length; i++){
+        answer[i] = left 
+        left = nums[i] * left 
+
+
+    }
+    for (let i = nums.length - 1; i >= 0; i--){
+        answer[i] *= right 
+        right *= nums[i]
+    }
+
+    return answer
+}
+console.log(productExceptSelf([1,2,3,4]))
+console.log(productExceptSelf([5,4]))
