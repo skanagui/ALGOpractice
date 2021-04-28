2dc4499bf915294dbf27a7295e574b1cc5e54b79
skanagui <s.kanagui@gmail.com>
2021-04-28 12:29:31 -0700

    hello

---
+++ Arrays/DynamicProgramming/Strings/IsPalimdrome.js
@@ -0,0 +1,38 @@
+/*
+
+Given a string s, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+
+ 
+
+Example 1:
+
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+Example 2:
+
+Input: s = "race a car"
+Output: false
+Explanation: "raceacar" is not a palindrome.
+ 
+
+Constraints:
+
+1 <= s.length <= 2 * 105
+s consists only of printable ASCII characters.
+*/
+
+const isPalindrome = (s) => {
+    // Have an original string that's lowercased and replaces all of the characters
+    let newOriginalString = s.replace(/[^A-Za-z0-9]/g, '').toLowerCase()
+    // Reverse the string
+    let reversedString = newOriginalString.split('').reverse().join('')
+    console.log("reversed: ", reversedString )
+    console.log("OG: ", newOriginalString )
+    // Return boolean value --> ternary or if()
+    return newOriginalString === reversedString ? true : false
+};
+
+
+
+console.log(isPalimdrome("A man, a plan, a canal: Panama"))
